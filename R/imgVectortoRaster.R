60d047fc8630c6229501852edd96b8f92176748d
howardbaik <howardhowiebaik@gmail.com>
2021-06-20 13:55:27 +0900

    Changed pos.loc to pos_loc

--- R/imgVectortoRaster.R
+++ R/imgVectortoRaster.R
@@ -1,26 +1,42 @@
-#load(datafile)
-#datalist <- out
 imgVectortoRaster <- function(centers, datalist){
+  
+  # n_K = number of rows in centers
   n_K <- nrow(centers)
+  
+  # Get out dirty data
   Data_dirty <- datalist$dat
+  # Latitude of dirty data
   lats <- Data_dirty[1,]
+  # Longitude of dirty data
   lons <- Data_dirty[2,]
+  # Aspect of dirty data
   asp <- c(table(lons)[1], table(lats)[1]) # lon (x) - lats (y)
-  bb.box <- c(min(lons), max(lons), min(lats), max(lats))
+  # bbox is a bounding box
+  bb_box <- c(min(lons), max(lons), min(lats), max(lats))
+  
+  # Populate non-NA columns (indicated by pos_loc) of centroid_images with centers
   centroid_images <- matrix(NA, n_K, ncol(Data_dirty))
-  centroid_images[, datalist$pos.loc] <- centers
+  centroid_images[, datalist$pos_loc] <- centers
   rownames(centroid_images) <- paste("Centroid", 1:n_K)
-  img.list <- list()
-  img.mat <- centroid_images
-  for(i in 1:nrow(img.mat)){
-    tmp <- matrix(img.mat[i,], asp[2], asp[1], byrow=TRUE)
+  
+  # Empty list 
+  img_list <- list()
+  # Assign img_mat
+  img_mat <- centroid_images
+  for(i in 1:nrow(img_mat)){
+    tmp <- matrix(img_mat[i,], asp[2], asp[1], byrow=TRUE)
     tmp <- tmp[asp[2]:1, ] # lat 7 at bottom not top
     tmp <- raster::raster(tmp)
-    raster::extent(tmp) <- bb.box
-    img.list[[i]] <- tmp
+    raster::extent(tmp) <- bb_box
+    img_list[[i]] <- tmp
   }
-  img.stack <- raster::stack(img.list)
-  names(img.stack) <- paste("Centroid", 1:n_K)
-  raster::crs(img.stack) <- "+proj=longlat +datum=WGS84"
-  return(list(list=img.list, stack=img.stack))
+  # Create a RasterStack object
+  img_stack <- raster::stack(img_list)
+  names(img_stack) <- paste("Centroid", 1:n_K)
+  # crs is a coordinate reference system
+  raster::crs(img_stack) <- "+proj=longlat +datum=WGS84"
+  
+  return(list(list=img_list, stack=img_stack))
+  
+  
 }
